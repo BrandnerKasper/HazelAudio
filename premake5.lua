b56cb30fbbaa21764db4bfc879c2138e0ac4c8b5
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2021-01-13 11:58:04 +0100

    edit premake file to work as submodule for Eternal Engine

--- premake5.lua
+++ premake5.lua
@@ -1,26 +1,6 @@
-workspace "HazelAudio"
-	architecture "x86_64"
-	startproject "HazelAudio-Examples"
-
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
-	
-	flags
-	{
-		"MultiProcessorCompile"
-	}
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-group "Dependencies"
-	include "HazelAudio/vendor/OpenAL-Soft"
-	include "HazelAudio/vendor/libogg"
-	include "HazelAudio/vendor/Vorbis"
-group ""
+include "HazelAudio/vendor/OpenAL-Soft"
+include "HazelAudio/vendor/libogg"
+include "HazelAudio/vendor/Vorbis"
 
 project "HazelAudio"
 	location "HazelAudio"
